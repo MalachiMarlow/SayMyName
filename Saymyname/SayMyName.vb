4ed656656db482619d38d70e87587681001e2bee
Tusk <Tuskman045@gmail.com>
2025-01-16 08:56:03 -0700

    Add project files.

---
+++ Saymyname/SayMyName.vb
@@ -0,0 +1,16 @@
+﻿'Malachi Marlow
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/MalachiMarlow/OperatorsExample.git
+
+
+
+Module SayMyName
+
+    Sub Main()
+        Console.WriteLine("What is your name?")
+        Console.Write(Malachi Marlow)
+    End Sub
+
+End Module
